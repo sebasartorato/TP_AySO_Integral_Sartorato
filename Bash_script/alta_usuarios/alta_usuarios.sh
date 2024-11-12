a339c58041e5a1621eb8568f557beb432e0c067e
sebasartorato <sebastiansartorato@gmail.com>
2024-11-12 18:52:57 +0000

    alta_usuarios

--- Bash_script/alta_usuarios/alta_usuarios.sh
+++ Bash_script/alta_usuarios/alta_usuarios.sh
@@ -14,14 +14,19 @@ clear
 ###############################
 
 LISTA=$1
+USER_PARAMETRO=$2
+USER_HASH=$(sudo grep "$USER_PARAMETRO" /etc/shadow | awk -F ':' '{print $2}')
 
 ANT_IFS=$IFS
 IFS=$'\n'
+
 for LINEA in `cat $LISTA |  grep -v ^#`
 do
 	USUARIO=$(echo  $LINEA |awk -F ':' '{print $1}')
 	GRUPO=$(echo  $LINEA |awk -F ':' '{print $2}')
-	echo "sudo useradd -m -s /bin/bash -g $GRUPO $USUARIO"
+	HOME_USER=$(echo  "$LINEA" |awk -F ':' '{print $3}')
+	sudo useradd -m -d "$HOME_USER" -s /bin/bash -p "$USER_HASH" -g "$GRUPO" "$USUARIO"
 done
+
 IFS=$ANT_IFS
 
